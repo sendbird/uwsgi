d512235511098f7d1bc8a8f35857d8dea3e93e91
Iggy Eom <iggy.eom@sendbird.com>
2020-10-28 14:28:01 +0900

    Revert continuation buffer support.

--- uwsgi.h
+++ uwsgi.h
@@ -1633,8 +1633,6 @@ struct wsgi_request {
 		struct sockaddr_in6 sin6;
 		struct sockaddr_un sun;
 	} client_addr;
-
-	uint8_t websocket_is_fin;
 };
 
 
@@ -2846,7 +2844,6 @@ struct uwsgi_server {
 	// uWSGI 2.0.19
 	int emperor_graceful_shutdown;
 	int is_chrooted;
-	struct uwsgi_buffer *websockets_continuation_buffer;
 
 	uint64_t max_worker_lifetime_delta;
 };
