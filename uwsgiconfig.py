ddb1f932914856cfd19f1584d42583f294d4e3f6
bellatoris <bellatoris@snu.ac.kr>
2022-07-11 19:04:14 +0900

    enable debug option to get all symbol tables

--- uwsgiconfig.py
+++ uwsgiconfig.py
@@ -1362,6 +1362,9 @@ class uConf(object):
         if self.get('unbit'):
             self.cflags.append("-DUNBIT")
 
+        # add debug symbol
+        self.cflags.append("-g")
+
         return self.gcc_list, self.cflags, self.ldflags, self.libs
 
 def build_plugin(path, uc, cflags, ldflags, libs, name = None):
